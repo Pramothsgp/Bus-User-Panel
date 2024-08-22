2018cc4b88afa3aedbf475258775b887876fe040
Pramothsgp <jarvis.tech.006@mail.com>
2024-08-23 01:52:49 +0530

    added navigation and styled it

--- src/Components/Navigation/NavBar.js
+++ src/Components/Navigation/NavBar.js
@@ -1,42 +1,40 @@
-import React, { useState } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
-import { IoClose, IoMenu } from "react-icons/io5";
 import "./NavBar.css";
 
 const NavBar = () => {
  return (
-   <header className="header">
      <nav className="nav-container">
-
+      {/* <IoClose className="nav__toggle"/> */}
        <div
          className={"nav__menu"}
          id="nav-menu"
        >
          <ul className="nav__list">
            <li className="nav__item">
-             <NavLink to="/" className="nav__link">
+             <NavLink to="/home-map" className="nav__link">
                Home
              </NavLink>
            </li>
            <li className="nav__item">
-             <NavLink to="/news" className="nav__link">
-               News
+             <NavLink to="/map" className="nav__link">
+               Bus Route
              </NavLink>
            </li>
            <li className="nav__item">
              <NavLink
-               to="/about-us"
+               to="/Deliverydetails"
                className="nav__link"
              >
-               About Us
+               HLD
              </NavLink>
            </li>
            <li className="nav__item">
              <NavLink
-               to="/favorite"
+               to="/orders"
                className="nav__link"
              >
-               Favorite
+               Orders
              </NavLink>
            </li>
            <li className="nav__item">
@@ -44,25 +42,17 @@ const NavBar = () => {
                to="/location"
                className="nav__link"
              >
-               Location
+               Help
              </NavLink>
            </li>
-           <li className="nav__item">
-             <NavLink to="/get-started" className="nav__link nav__cta">
-               Get Started
-             </NavLink>
-           </li>
+
          </ul>
          <div className="nav__close" id="nav-close">
-           <IoClose />
+           
          </div>
        </div>
 
-       <div className="nav__toggle" id="nav-toggle">
-         <IoMenu />
-       </div>
      </nav>
-   </header>
  );
 };
 
