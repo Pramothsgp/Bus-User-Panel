caf49a492a2d2d5695fa2b9ea38af51feb9e509d
Pramothsgp <jarvis.tech.006@mail.com>
2024-08-22 12:18:47 +0530

    Updated Local Deliver  Panel

--- src/Components/LocalDelivery/DeliveryDetails.js
+++ src/Components/LocalDelivery/DeliveryDetails.js
@@ -7,20 +7,37 @@ const locationData = [
   { id :"kun",name: 'kuniyamuthur', latitude: 10.96324600, longitude: 76.94702200 },
   { id:"kvp",name: 'kovaipudur perivu', latitude: 10.93676, longitude: 76.951173 }
 ];
+const num = '1234567890';
+const alpb = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const DeliveryDetails = () => {
-  
+  const [orderid, setOrderid] = useState('');
+  const [price, setPrice] = useState(0);
+  const [weight, setweight] = useState(null);
   const [pickupLocation, setPickupLocation] = useState('');
   const [destination, setDestination] = useState('');
+  const [showOrder, setShowOrder] = useState(false);
+  const handleSubmit = ()=>{
+    var temp = '';
+    for (var i = 0; i < 2; i++)
+      temp += alpb[Math.floor(Math.random() * 26)];
+    for ( i = 0; i < 3; i++)
+      temp += num[Math.floor(Math.random() * 10)];
+
+    setOrderid(temp);
+    setPrice(Math.round(weight * 2.76 * 100)/100);
+    setShowOrder(true);
+  }
   return (
-    <div className='HLP-container'>
-      <form className='HLP-form'>
+    <div className='HLP-container' >
+      <h1><center>Hyper Local Delivery</center></h1>
+      <div className='HLP-form' >
         <div className='HLP-searchbar-container'>
           <SearchBar 
             inputValue={pickupLocation}
             setInputValue={setPickupLocation}
             allSuggestions={locationData}
             attribute='name'
-            label= 'Pick Up Location'
+            label='Pick Up Location'
           />
           </div>
         <div className='HLP-searchbar-container'>
@@ -32,8 +49,37 @@ const DeliveryDetails = () => {
             label= 'Delivery Location'
           />
         </div>
-        <button className='HLP-form-btn'>Submit</button>
-      </form>
+        <div className='HLP-weight-container'>
+          <div className='HLP-weight-label'>
+            <label htmlFor='weight-input'>Enter Weight</label>
+          </div>
+          <input  
+            id='weight-input'
+            type='number'
+            placeholder='Enter Weight'
+            onChange={(e) => {
+              setweight(e.target.value);
+              setShowOrder(false);
+            }}
+          />
+        </div>
+        {!showOrder &&
+          <button
+            className='HLP-form-btn'
+            onClick={handleSubmit}
+          >Submit</button>
+        }
+      </div>
+      {showOrder &&
+        <div className='HLP-weight-container'>
+          <h3>
+            Order id : {orderid}
+          </h3>
+          <h3>
+            Price : {price}
+          </h3>
+        </div>
+      }
     </div>
   );
 };
